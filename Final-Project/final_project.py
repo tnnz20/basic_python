4f724c8c5573aecb75aff8133b4241aca33259b7
tnnz20 <kaitora200@gmail.com>
2021-01-31 15:03:38 +0800

    Final Project

--- Final-Project/final_project.py
+++ Final-Project/final_project.py
@@ -4,21 +4,39 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-#parsing file txt untuk digunakan dalam python
-file = open('Final-Project/email.txt', 'r').read()
+#----------Sumber------------
+#belajarpython.com "parsing txt"
+#realpython.com/python-send-email "penggunaan modul email"
+#qastack.id/programming/3362600/how-to-send-email-attachments "attachment"
+
+#parsing file txt untuk penerima mail menggunakan file txt
+file = open('Final-Project/receiver_list.txt', 'r').read()
 flx = file.split()
 
 #deklarasi inputan untuk mail
 sender_email = "sulthan.a2.pilkom@gmail.com"
 rec_email = flx
-body = "Ini tugas terakhir\ncoba"
+attach = open("Final-Project/pilar.jpg", "rb").read()
+body = """\
+Ini dikirim melalu SMPT Python
+Sebagai Tugas Terakhir
 
+
+Terimakasih :)
+"""
+html ="""\
+<html>
+    <body>
+        <h1>----------Pesan Html----------</h1>
+        <p style="color:red;">Warna Merah</p>
+    </body>
+</html>
+"""
 #melampirkan suatu file
 part = MIMEBase('application', "octet-stream")
-part.set_payload(open("Final-Project/pilar.jpg", "rb").read())
+part.set_payload(attach)
 encoders.encode_base64(part)
-
-part.add_header('Content-Disposition', 'attachment; filename="Final-Project/pilar.jpg"')
+part.add_header('Content-Disposition', 'attachment; filename="pilar.jpg"')
 
 #draf mail
 msg = MIMEMultipart()
@@ -28,6 +46,7 @@ msg['Subject'] = 'Final Project'
 
 #input text dan lampiran ke mail
 msg.attach(MIMEText(body, 'plain'))
+msg.attach(MIMEText(html,'html'))
 msg.attach(part)
 text = msg.as_string()
 
@@ -37,8 +56,12 @@ server.starttls()
 
 #login user
 password = input(str("Masukan Password :"))
-server.login(sender_email, password)
-#kirim mail
-server.sendmail(sender_email, rec_email, text)
+try:
+    server.login(sender_email, password)
+    #kirim mail
+    server.sendmail(sender_email, rec_email, text)
+    print("Mail Berhasil Dikirim")
+except:
+    print("Mail gagal untuk dikirim coba lagi")
 #keluar dari server
 server.quit()
---
+++ Final-Project/tes.py
@@ -0,0 +1,3 @@
+f = open("Final-Project/receiver_list.txt","r").read().split()
+
+print(f)
